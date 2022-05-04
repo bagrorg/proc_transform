8ae5d96c86fb5fa7399477ab502c407db47eb956
bagrorg <kuldanvit@gmail.com>
2022-05-04 20:35:05 +0300

    splitted to two versions and splitted tests for them

--- tests/test.cpp
+++ tests/test.cpp
@@ -3,35 +3,35 @@
 #include "../utils/Transform.h"
 #include <numeric>
 
-TEST(TransformTests, SuccTestSolo) {
+TEST(TransformTestsDynamic, SuccTestSolo) {
     auto func = [](int x) {
-        return x + 1 == 0;
+        return x + 1;
     };
 
     std::vector<int> v = {
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10
     };
-    auto ans = TransformWithProcesses(v, func, 1);
+    auto ans = TransformWithProcesses<DYN>(v, func, 1);
     for (int i = 0; i < v.size(); i++) {
         ASSERT_EQ(ans[i], func(v[i]));
     }
 }
 
-TEST(TransformTests, SuccTest) {
+TEST(TransformTestsDynamic, SuccTest) {
     auto func = [](int x) {
-        return x + 1 == 0;
+        return x + 1;
     };
 
     std::vector<int> v = {
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10
     };
-    auto ans = TransformWithProcesses(v, func, 3);
+    auto ans = TransformWithProcesses<DYN>(v, func, 3);
     for (int i = 0; i < v.size(); i++) {
         ASSERT_EQ(ans[i], func(v[i]));
     }
 }
 
-TEST(TransformTests, DifferentTypeTest) {
+TEST(TransformTestsDynamic, DifferentTypeTest) {
     auto func = [](int x) {
         return x % 2 == 0;
     };
@@ -39,20 +39,75 @@ TEST(TransformTests, DifferentTypeTest) {
     std::vector<int> v = {
         1, 2, 3, 4, 5, 6, 7, 8, 9, 10
     };
-    auto ans = TransformWithProcesses(v, func, 3);
+    auto ans = TransformWithProcesses<DYN>(v, func, 3);
     for (int i = 0; i < v.size(); i++) {
         ASSERT_EQ(ans[i], func(v[i]));
     }
 }
 
-TEST(TransformTests, BigTest) {
+TEST(TransformTestsDynamic, BigTest) {
     auto func = [](int x) {
         return x * x * x + 2 * x * x - x * 4 - 1;
     };
 
     std::vector<int> v(10000);
     std::iota(v.begin(), v.end(), 0);
-    auto ans = TransformWithProcesses(v, func, 3);
+    auto ans = TransformWithProcesses<DYN>(v, func, 3);
+    for (int i = 0; i < v.size(); i++) {
+        ASSERT_EQ(ans[i], func(v[i]));
+    }
+}
+
+TEST(TransformTestsStatic, SuccTestSolo) {
+    auto func = [](int x) {
+        return x + 1;
+    };
+
+    std::vector<int> v = {
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    };
+    auto ans = TransformWithProcesses<STAT>(v, func, 1);
+    for (int i = 0; i < v.size(); i++) {
+        ASSERT_EQ(ans[i], func(v[i]));
+    }
+}
+
+TEST(TransformTestsStatic, SuccTest) {
+    auto func = [](int x) {
+        return x + 1;
+    };
+
+    std::vector<int> v = {
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    };
+    auto ans = TransformWithProcesses<STAT>(v, func, 3);
+    for (int i = 0; i < v.size(); i++) {
+        ASSERT_EQ(ans[i], func(v[i]));
+    }
+}
+
+TEST(TransformTestsStatic, DifferentTypeTest) {
+    auto func = [](int x) {
+        return x % 2 == 0;
+    };
+
+    std::vector<int> v = {
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    };
+    auto ans = TransformWithProcesses<STAT>(v, func, 3);
+    for (int i = 0; i < v.size(); i++) {
+        ASSERT_EQ(ans[i], func(v[i]));
+    }
+}
+
+TEST(TransformTestsStatic, BigTest) {
+    auto func = [](int x) {
+        return x * x * x + 2 * x * x - x * 4 - 1;
+    };
+
+    std::vector<int> v(10000);
+    std::iota(v.begin(), v.end(), 0);
+    auto ans = TransformWithProcesses<STAT>(v, func, 3);
     for (int i = 0; i < v.size(); i++) {
         ASSERT_EQ(ans[i], func(v[i]));
     }
--- utils/Transform.h
+++ utils/Transform.h
@@ -7,25 +7,30 @@
 #include <iostream>
 #include <wait.h>
 
+enum TransformVersion { STAT, DYN };
+
+template <TransformVersion V>
+struct Tag {};
+
 template <typename R>
 concept InputContiguousRange = std::ranges::contiguous_range<R>;
 
 template <typename F, typename A>
 concept StatelessFunction = std::invocable<F, A>; //?
 
-template <typename R,  
+template <typename R,
           typename F,
           typename A = std::ranges::range_value_t<R>,
           typename B = typename std::invoke_result<F, A>::type,
           typename std::enable_if<std::is_trivially_copyable<B>::value, bool>::type = true>
 requires (InputContiguousRange<R>, StatelessFunction<F, A>, std::ranges::range<SharedArray<B>>)
-void worker_private(SharedArray<B>& data_new, R& data_old, size_t work_size, size_t cur_size, size_t total_size, F f) {
-    for (int i = cur_size; i < cur_size + work_size && i < total_size; i++) {
+void worker_private(SharedArray<B>& data_new, R& data_old, size_t position, size_t work_size, F f) {
+    for (size_t i = position; i < position + work_size && i < data_old.size(); i++) {
         data_new.set(i, f(data_old.data()[i]));
     }
 }
 
-template <typename R,  
+template <typename R,
           typename F,
           typename A = std::ranges::range_value_t<R>,
           typename B = typename std::invoke_result<F, A>::type,
@@ -50,13 +55,49 @@ void dynamic_worker_private(SharedArray<B>& data_new, R& data_old, F f, SharedAt
     }
 }
 
-template <typename R,  
+template <typename R,
           typename F,
           typename A = std::ranges::range_value_t<R>,
           typename B = typename std::invoke_result<F, A>::type,
           typename std::enable_if<std::is_trivially_copyable<B>::value, bool>::type = true>
 requires (InputContiguousRange<R>, StatelessFunction<F, A>, std::ranges::range<SharedArray<B>>)
-SharedArray<B> TransformWithProcesses(R&& range, F&& f, size_t nprocesses) {
+SharedArray<B> TransformWithProcesses(R&& range, F&& f, size_t nprocesses, Tag<STAT>) {
+    size_t range_size = range.size();
+    size_t work_size = std::ceil((float) range_size / nprocesses);
+    size_t position = 0;
+
+    SharedArray<B> arr(range_size);
+
+    for (int i = 0; i < nprocesses; i++) {
+        pid_t pid = fork();
+
+        switch (pid) {
+            case 0: {
+                worker_private(arr, range, position, work_size, f);
+                exit(EXIT_SUCCESS); // TODO ??
+            }
+            case -1:
+                std::cerr << "Something went wrong" << std::endl;
+                exit(EXIT_FAILURE); // TODO operate safier: kill all childs? continue?
+            default:
+                position += work_size;
+                break;
+        }
+    }
+    pid_t wpid;
+    int status = 0;
+    while ((wpid = wait(&status)) > 0);
+
+    return arr;
+}
+
+template <typename R,
+          typename F,
+          typename A = std::ranges::range_value_t<R>,
+          typename B = typename std::invoke_result<F, A>::type,
+          typename std::enable_if<std::is_trivially_copyable<B>::value, bool>::type = true>
+requires (InputContiguousRange<R>, StatelessFunction<F, A>, std::ranges::range<SharedArray<B>>)
+SharedArray<B> TransformWithProcesses(R&& range, F&& f, size_t nprocesses, Tag<DYN>) {
     size_t range_size = range.size();
     size_t work_size = std::ceil((float) range_size / nprocesses);
     size_t cur_size = 0;
@@ -85,4 +126,15 @@ SharedArray<B> TransformWithProcesses(R&& range, F&& f, size_t nprocesses) {
     while ((wpid = wait(&status)) > 0);
 
     return arr;
+}
+
+template <TransformVersion V,
+          typename R,
+          typename F,
+          typename A = std::ranges::range_value_t<R>,
+          typename B = typename std::invoke_result<F, A>::type,
+          typename std::enable_if<std::is_trivially_copyable<B>::value, bool>::type = true>
+requires (InputContiguousRange<R>, StatelessFunction<F, A>, std::ranges::range<SharedArray<B>>)
+SharedArray<B> TransformWithProcesses(R&& range, F&& f, size_t nprocesses) {
+    return TransformWithProcesses(range, f, nprocesses, Tag<V>{});
 }
